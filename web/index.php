37af8dd1af63660c81c1a0185434709a81196987
stonebinox <anoop.santhanam@gmail.com>
2017-10-26 13:25:53 +0530

    Setting up files

--- web/index.php
+++ web/index.php
@@ -1,11 +1,30 @@
 <?php
-
 ini_set('display_errors', 1);
-
 require_once __DIR__.'/../vendor/autoload.php';
-
 $app = require __DIR__.'/../src/app.php';
 require __DIR__.'/../config/prod.php';
 require __DIR__.'/../src/controllers.php';
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+      'driver' => 'pdo_mysql',
+      'dbname' => 'heroku_39e3086099b39f3',
+      'user' => 'b66219b6e365b3',
+      'password' => '4efa1857',
+      'host'=> "us-cdbr-iron-east-05.cleardb.net",
+    )
+));
+$app->register(new Silex\Provider\SessionServiceProvider, array(
+    'session.storage.save_path' => dirname(__DIR__) . '/tmp/sessions'
+));
+$app->before(function(Request $request) use($app){
+    $request->getSession()->start();
+});
 $app->run();
