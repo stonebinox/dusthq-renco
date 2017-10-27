a21e945b7f6a52768cd3c6c1632f18f5cc6f39c8
stonebinox <anoop.santhanam@gmail.com>
2017-10-27 14:20:31 +0530

    Homepage in the making

--- web/js/renco.js
+++ web/js/renco.js
@@ -1,6 +1,7 @@
 var app=angular.module("renco",[]);
 app.controller("pre-book",function($scope,$http,$compile){
     $scope.itemArray=[];
+    $scope.totalCost=399;
     $scope.getItems=function(){
         $http.get("getItems")
         .then(function success(response){
@@ -42,15 +43,19 @@ app.controller("pre-book",function($scope,$http,$compile){
             $("#itemlist").html(h3);
             $("#itemlist").css("display","block");
             var form=document.createElement("form");
+            $(form).attr("name","prebook");
+            $(form).attr("method","post");
+            $(form).attr("action","book");
             for(var i=0;i<items.length;i++){
                 var item=items[i];
                 var itemID=item.iditem_master;
                 var itemName=stripslashes(item.item_name);
                 var itemPrice=item.item_price;
                 var formGroup=document.createElement("div");
+                $(formGroup).addClass("form-group");
                     var label=document.createElement("label");
                     $(label).attr("for","item"+itemID);
-                    $(label).html(itemName+'&nbsp;<span class="badge">{{range'+itemID+'}}</span>');
+                    $(label).html(itemName+'&nbsp;<span class="badge" id="range'+itemID+'">{{range'+itemID+'}}</span>');
                     $(formGroup).append(label);
                     var range=document.createElement("input");
                     $(range).attr("type","range");
@@ -64,10 +69,62 @@ app.controller("pre-book",function($scope,$http,$compile){
                     $(formGroup).append(range);
                 $("#itemlist").append(formGroup);
             }
+            var formGroupEmail=document.createElement("div");
+            $(formGroupEmail).addClass("form-group");
+                var emailLabel=document.createElement("label");
+                $(emailLabel).html("Email");
+                $(emailLabel).attr("for","user_email");
+                $(formGroupEmail).append(emailLabel);
+                var emailField=document.createElement("input");
+                $(emailField).attr("type","email");
+                $(emailField).attr("name","user_email");
+                $(emailField).attr("id","user_email");
+                $(emailField).attr("placeholder","Enter a valid email ID");
+                $(emailField).addClass("form-control");
+                $(emailField).attr("required","true");
+                $(formGroupEmail).append(emailField);
+            $("#itemlist").append(formGroupEmail);
+            var formGroupNumber=document.createElement("div");
+            $(formGroupNumber).addClass("form-group");
+                var numberLabel=document.createElement("label");
+                $(numberLabel).html("Mobile (optional)");
+                $(numberLabel).attr("for","user_mobile");
+                $(formGroupNumber).append(numberLabel);
+                var numberField=document.createElement("input");
+                $(numberField).attr("type","tel");
+                $(numberField).attr("name","user_mobile");
+                $(numberField).attr("id","user_mobile");
+                $(numberField).attr("placeholder","Enter a valid mobile number");
+                $(numberField).addClass("form-control");
+                $(formGroupNumber).append(numberField);
+            $("#itemlist").append(formGroupNumber);
+            var buttonHolder=document.createElement("div");
+            $(buttonHolder).addClass("text-center");
+                var button=document.createElement("button");
+                $(button).attr("type","button");
+                $(button).addClass("btn btn-primary btn-lg");
+                $(button).html('Book for <span id="price">{{totalCost}}</span> kr./gang');
+                $(buttonHolder).html(button);
+            $("#itemlist").append(buttonHolder);
             $compile("#itemlist")($scope);
         }
     };
-    $scope.getPrice=function(){
-
+    $scope.getPrice=function(itemID){
+        var itemQuantity=parseInt($("#range"+itemID).text());
+        var items=$scope.itemArray.slice();
+        var pos=null;
+        for(var i=0;i<items.length;i++){
+            var item=items[i];
+            if(item.iditem_master==itemID){
+                pos=i;
+                break;
+            }
+        }
+        if(pos!=null){
+            var item=items[pos];
+            var itemPrice=item.item_price;
+            var cost=itemQuantity*itemPrice;
+            $scope.totalCost=cost;
+        }
     };
 });
